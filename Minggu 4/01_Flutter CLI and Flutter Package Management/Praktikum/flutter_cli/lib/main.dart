38dd2ee17753e455cf885149ac4392291cd4e521
Muhammad Albert Nur Agathon <muhammadalbert16@gmail.com>
2023-09-06 10:17:40 +0700

    add answer task priority 1&2

---
+++ Minggu 4/01_Flutter CLI and Flutter Package Management/Praktikum/flutter_cli/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Halo Alterra"),
+        backgroundColor: Colors.cyan,
+      ),
+      body: const Center(
+        child: Text("Nama saya adalah Muhammad Albert Nur Agathon", style: TextStyle(fontSize: 16),),
+      ),
+    );
+  }
+}
