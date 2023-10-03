2463959de6054862f2d2b3dce063d37991b5bc67
Muhammad Albert Nur Agathon <muhammadalbert16@gmail.com>
2023-10-03 18:20:38 +0700

    add splash screen

--- Minggu 7/01_State Management Provider/Praktikum/flutter_provider/lib/main.dart
+++ Minggu 7/01_State Management Provider/Praktikum/flutter_provider/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_provider/provider/contact_provider.dart';
+import 'package:flutter_provider/routes/routes_navigation.dart';
 import 'package:flutter_provider/screen/contact_screen.dart';
+import 'package:flutter_provider/screen/splash_screen.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -17,16 +19,16 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Advance Flutter Form',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const ContactScreen(),
+      initialRoute: RoutesNavigation.splashScreen,
+      routes: {
+        RoutesNavigation.splashScreen: (_) => const SplashScreen(),
+        RoutesNavigation.contactScreen: (_) => const ContactScreen(),
+      },
     );
   }
 }
